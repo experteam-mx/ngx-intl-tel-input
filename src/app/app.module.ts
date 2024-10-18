0b8c05a40fd2c1966437606653f33b0bd79d605b
Adel López <adellopezpantoja@gmail.com>
2024-10-18 16:15:55 -0400

    feat: CRA10-9343

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
-import {
-	NgxIntlTelInputModule
-} from 'projects/ngx-intl-tel-input/src/lib/ngx-intl-tel-input.module';
-
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { NgxIntlTelInputModule } from 'ngx-intl-tel-input';
 
 @NgModule({
 	declarations: [AppComponent],
